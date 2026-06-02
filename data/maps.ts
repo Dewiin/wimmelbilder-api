d1e4c27ed9138db5788cc3e6aa54d3fd38eb12a0
Devin Xie <devinxie418@gmail.com>
2026-06-01 22:50:26 -0400

    feat: add universe11 characters

---
+++ data/maps.ts
@@ -0,0 +1,17 @@
+export const maps = [
+  {
+    name: "spacecon",
+    imageUrl: "/maps/spacecon.jpg",
+    description: "A futuristic sci-fi convention packed with aliens and hidden travelers.",
+  },
+  {
+    name: "undrcty",
+    imageUrl: "/maps/undrcty.jpg",
+    description: "A dense underground cyberpunk city filled with strange activity.",
+  },
+  {
+    name: "universe11",
+    imageUrl: "/maps/universe11.jpg",
+    description: "A chaotic multiverse battlefield with heroes scattered everywhere.",
+  },
+];
---
+++ data/universe11.ts
@@ -0,0 +1,83 @@
+export const universe11Characters = [
+    {
+        name: "Groot",
+        xMin: 0.54,
+        xMax: 0.58,
+        yMin: 0.76,
+        yMax: 0.78,
+        imageUrl: "/characters/groot.png",
+        mapId: "cmpdhl6ae0002k8f3n38hctt2"
+    },
+    {
+        name: "Genos",
+        xMin: 0.28,
+        xMax: 0.30,
+        yMin: 0.63,
+        yMax: 0.65,
+        imageUrl: "/characters/genos.png",
+        mapId: "cmpdhl6ae0002k8f3n38hctt2"
+    },
+    {
+        name: "Luffy",
+        xMin: 0.63,
+        xMax: 0.66,
+        yMin: 0.82,
+        yMax: 0.85,
+        imageUrl: "/characters/luffy.png",
+        mapId: "cmpdhl6ae0002k8f3n38hctt2"
+    },
+    {
+        name: "Guts",
+        xMin: 0.47,
+        xMax: 0.52,
+        yMin: 0.88,
+        yMax: 0.92,
+        imageUrl: "/characters/guts.png",
+        mapId: "cmpdhl6ae0002k8f3n38hctt2"
+    },
+    {
+        name: "Minion",
+        xMin: 0.39,
+        xMax: 0.42,
+        yMin: 0.61,
+        yMax: 0.64,
+        imageUrl: "/characters/minion.png",
+        mapId: "cmpdhl6ae0002k8f3n38hctt2"
+    },
+    {
+        name: "Griffith",
+        xMin: 0.04,
+        xMax: 0.07,
+        yMin: 0.45,
+        yMax: 0.50,
+        imageUrl: "/characters/griffith.png",
+        mapId: "cmpdhl6ae0002k8f3n38hctt2"
+    },
+    {
+        name: "Johnny Bravo",
+        xMin: 0.31,
+        xMax: 0.38,
+        yMin: 0.35,
+        yMax: 0.41,
+        imageUrl: "/characters/johnny_bravo.png",
+        mapId: "cmpdhl6ae0002k8f3n38hctt2"
+    },
+    {
+        name: "Sanic",
+        xMin: 0.79,
+        xMax: 0.85,
+        yMin: 0.65,
+        yMax: 0.68,
+        imageUrl: "/characters/sanic.png",
+        mapId: "cmpdhl6ae0002k8f3n38hctt2"
+    },
+    {
+        name: "Saitama",
+        xMin: 0.32,
+        xMax: 0.38,
+        yMin: 0.60,
+        yMax: 0.63,
+        imageUrl: "/characters/saitama.png",
+        mapId: "cmpdhl6ae0002k8f3n38hctt2"
+    },
+]
